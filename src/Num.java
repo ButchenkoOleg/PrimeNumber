da9881b84008087751f044e0d15de2e63622d2ab
oleg <butchenkooleg@gmail.com>
2017-09-07 10:12:47 +0300

    naw br

--- src/Num.java
+++ src/Num.java
@@ -7,22 +7,8 @@ public class Num {
         int palindr = 0;
         int counter = 0;
         for ( ; j > i; j=j-2){
-            if (j%3 != 0){
-                if (j%5 != 0){
-                    if (j%7 != 0){
-                        if (j%11 != 0 )
-                            for (int k = 13; k <= j; k=k+2){
-                            if (j%k != 0){
-                                counter++;
-                                System.out.println(j);
-                                break;
-                            }
-                            }
 
-                    }
-                }
-
-            }
+            System.out.println(j);
 
         }
 
